fe0e7e995492eadc6b3b6e5de6bda9a5d8b28693
leTuong05 <vantuongno1@gmail.com>
2023-07-27 13:54:16 +0700

    Page San pham (Trang chu)

--- src/services/apis/products.js
+++ src/services/apis/products.js
@@ -1,7 +1,11 @@
 import axiosInstance from "../axios";
 
-export const getAllProduct = (body) =>
-  axiosInstance.post("Guest/GetAllProduct", body);
+export const getAllProduct = (PageSize,CurrentPage,SortType) =>
+  axiosInstance.get(`Product?PageSize=${PageSize}&CurrentPage=${CurrentPage}&SortType=${SortType}`);
 
 export const getProductDetail = (id) =>
-  axiosInstance.get(`Guest/GetProductDetails?ProductID=${id}`);
+  axiosInstance.get(`Product/GetByID?id=${id}`);
+
+export const getProductBestSell= ()=>{
+  return axiosInstance.get('Product/ProductBestSell')
+}
