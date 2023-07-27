5b62db004e5f3ebfbba199545f787b1e52563c44
hoangquan2012 <88789218+hoangquan2012@users.noreply.github.com>
2023-07-27 15:34:10 +0700

    update

--- src/services/apis/products.js
+++ src/services/apis/products.js
@@ -5,12 +5,10 @@ export const getAllProduct = () => axiosInstance.get('Product');
 export const getAdminProduct = () => axiosInstance.get(`Product/Manage`);
 
 export const adminAddProduct = () => axiosInstance.post(`Product/Manage/Insert`);
-export const getAllProduct = (PageSize,CurrentPage,SortType) =>
-  axiosInstance.get(`Product?PageSize=${PageSize}&CurrentPage=${CurrentPage}&SortType=${SortType}`);
+export const getAllProduct1 = (PageSize, CurrentPage, SortType) => axiosInstance.get(`Product?PageSize=${PageSize}&CurrentPage=${CurrentPage}&SortType=${SortType}`);
 
-export const getProductDetail = (id) =>
-  axiosInstance.get(`Product/GetByID?id=${id}`);
+export const getProductDetail = (id) => axiosInstance.get(`Product/GetByID?id=${id}`);
 
-export const getProductBestSell= ()=>{
-  return axiosInstance.get('Product/ProductBestSell')
-}
+export const getProductBestSell = () => {
+    return axiosInstance.get('Product/ProductBestSell');
+};
