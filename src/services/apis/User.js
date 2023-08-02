d86fd05cb549de48b3969099cefce8ee6ccea679
leTuong05 <vantuongno1@gmail.com>
2023-08-02 17:01:33 +0700

    update

--- src/services/apis/User.js
+++ src/services/apis/User.js
@@ -1,23 +1,3 @@
-import axiosInstance from "../axios";
+import axiosInstance from '../axios';
 
-export const getListPhonebookAgent = (body) => {
-    return axiosInstance.post('User/GetAgent', body)
-}
-export const getListPhonebookGuest = (body) => {
-    return axiosInstance.post('User/GetGuest', body)
-}
-
-export const InsertGuest = (body) => {
-    return axiosInstance.post('User/InsertGuest', body)
-}
-export const InserAgent = (body) => {
-    return axiosInstance.post('User/InsertAgent', body)
-}
-
-export const DeleteGuest = (UserID) => {
-    return axiosInstance.patch(`User/DeleteGuest?UserID=${UserID}`)
-}
-
-export const UpdateGuest=(body)=>{
-    return axiosInstance.put('User/UpdateGuest',body)
-}
+export const getListStaff = () => axiosInstance.get('User/GetListStaff');
