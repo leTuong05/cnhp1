01d86894d29efa33aa321cc99ab12c3ba19d4c9c
Thang Van <85693841+UnbertenKma@users.noreply.github.com>
2023-08-03 10:32:12 +0700

    update

--- src/services/apis/user.js
+++ src/services/apis/user.js
@@ -1,3 +1,23 @@
-import axiosInstance from '../axios';
+import axiosInstance from "../axios";
 
-export const getListStaff = () => axiosInstance.get('User/GetListStaff');
+export const getListPhonebookAgent = (body) => {
+    return axiosInstance.post('User/GetAgent', body)
+}
+export const getListPhonebookGuest = (body) => {
+    return axiosInstance.post('User/GetGuest', body)
+}
+
+export const InsertGuest = (body) => {
+    return axiosInstance.post('User/InsertGuest', body)
+}
+export const InserAgent = (body) => {
+    return axiosInstance.post('User/InsertAgent', body)
+}
+
+export const DeleteGuest = (UserID) => {
+    return axiosInstance.patch(`User/DeleteGuest?UserID=${UserID}`)
+}
+
+export const UpdateGuest=(body)=>{
+    return axiosInstance.put('User/UpdateGuest',body)
+}
